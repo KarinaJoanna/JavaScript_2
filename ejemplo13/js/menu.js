e503c20ece43a808d9cb004876af2918a5f9f4b6
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2022-11-15 18:00:58 -0600

    ex13

--- ejemplo13/js/menu.js
+++ ejemplo13/js/menu.js
@@ -1,19 +1,3 @@
-class Boton {
-    constructor(contenedorElemento, texto) {
-    this.contenedorElemento = contenedorElemento;
-    this.texto = texto;
-    this.onClick = this.onClick.bind(this);
-    const boton = document.createElement('button');
-    boton.textContent = texto;
-    boton.addEventListener('click',this.onClick)
-    this.contenedorElemento.append(boton);
-    }
-    onClick(){
-        const eventInfo = { nombreBoton: this.texto }; document.dispatchEvent(new CustomEvent('boton-click',{detail:
-        eventInfo}));
-        }
-    }
-    
     class Menu {
         constructor() {
         this.botonContenedor = document.querySelector('#menu');
