5d7b610c522901f65783f2a4033ea6afd1b43fc5
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2022-11-15 18:02:51 -0600

    ex15

---
+++ ejemplo15/js/aplicacion.js
@@ -0,0 +1,15 @@
+class Aplicacion {
+    constructor(contenedorRegalo, contenedorTitulo) {
+    this.contenedorRegalo = contenedorRegalo; 
+    this.contenedorTitulo = contenedorTitulo; 
+    this.regalos = [];
+    this.cargarContenedorRegalo();
+    }
+
+    cargarContenedorRegalo() {
+        for(const enlace of REGALOS_ENLACES){
+            const regalo = new Regalo(this.contenedorRegalo, enlace);
+            this.regalos.push(regalo);
+        }
+    }
+}
