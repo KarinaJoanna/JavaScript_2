e52eef89c36a29450f6b63d64d6a92c009a8305a
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2022-11-17 18:57:23 -0600

    ex16

---
+++ ejemplo16/js/main.js
@@ -0,0 +1,4 @@
+const albumview = document.querySelector('#album-view')
+const almbum2 = document.querySelector('#album-view2')
+const album1 = new Album(albumview, PHOTO_LIST)
+const album2 = new Album(almbum2, REGALOS_ENLACES)
